51a6451776abbd7d65762cc35aa984c7eefc26fc
7PXS <godx00@outlook.com>
2025-04-28 17:31:26 -0700

    Create Remotes.lua

---
+++ Jujitsu-Infinate/Features/Auto Spin/Remotes.lua
@@ -0,0 +1,25 @@
+-- Remotes Here :)
+
+local Remotes = {
+
+  PvP = {      
+        M1 = "",
+          M2 = "",
+            Parry = "",
+              Fait = "",
+                Dash = "",
+                  SkyDive = "",
+  },
+
+  Missions = {
+        Quest1 = "",
+          Quest2 = "",
+            Quest3 = "",
+  }
+
+
+
+
+}
+
+return Remotes
