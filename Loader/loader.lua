b2a93fdb67d99d0b15b5858e1ae3289699937f73
7PXS <godx00@outlook.com>
2025-05-04 20:03:41 -0700

    Update loader.lua

--- Loader/loader.lua
+++ Loader/loader.lua
@@ -3,7 +3,8 @@ local script_key = getgenv().scriptkey
 local gameScripts = {
     [4483381587] = {
         name = "TEst",
-        scriptUrl = "https://api.luarmor.net/files/v3/loaders/33b73adf41e86b60864d731dc77f9150.lua"
+        scriptUrl = "script_key = getgenv().scriptkey
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/33b73adf41e86b60864d731dc77f9150.lua"))()"
     }
 }
 
@@ -21,7 +22,7 @@ local function loadGameScript()
         return
     end
     
-    loadstring(game:HttpGet(scriptData.scriptUrl))(script_key)
+    loadstring(game:HttpGet(scriptData.scriptUrl))()
 end
 
 loadGameScript()
