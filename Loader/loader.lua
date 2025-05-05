7681ccda3d592ccabf0fce6aac3c018ab2a2017e
7PXS <godx00@outlook.com>
2025-05-04 20:18:38 -0700

    Update loader.lua

--- Loader/loader.lua
+++ Loader/loader.lua
@@ -2,27 +2,25 @@ local script_key = getgenv().scriptkey
 
 local gameScripts = {
     [4483381587] = {
-        name = "TEst",
-        scriptUrl = "script_key = getgenv().scriptkey
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/33b73adf41e86b60864d731dc77f9150.lua"))()"
+        name = "Test",
+        scriptUrl = "https://api.luarmor.net/files/v3/loaders/33b73adf41e86b60864d731dc77f9150.lua"
     }
 }
 
-local function loadGameScript()
     if not script_key then
         game.Players.LocalPlayer:Kick("Invalid script key")
         return
     end
-    
+
     local placeId = game.PlaceId
     local scriptData = gameScripts[placeId]
-    
+
     if not scriptData then
         game.Players.LocalPlayer:Kick("No script available for this game")
         return
     end
-    
-    loadstring(game:HttpGet(scriptData.scriptUrl))()
-end
 
-loadGameScript()
+    getgenv().script_key = script_key
+
+    loadstring(game:HttpGet(scriptData.scriptUrl))()
+
