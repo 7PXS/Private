a0552ca56e4c90934a98cd236e6caf38766aac3c
7PXS <godx00@outlook.com>
2025-05-13 11:17:21 -0700

    Create QuestData.lua

---
+++ GPO/Data/QuestData.lua
@@ -0,0 +1,8 @@
+local Data = {
+          Daph = {
+            Pos = "", -- Optional But Should Deafult to this if cannt find npcc
+            MobName = "Bandit",
+            level = 1 - 5, -- do this quest only for level 1 through 5
+            QuestName  = "Help Daph"
+  }
+}
