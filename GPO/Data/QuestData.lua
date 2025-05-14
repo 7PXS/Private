0a3e0074c93b91cd93bef4bb36f497c2cd81f87f
7PXS <godx00@outlook.com>
2025-05-14 16:55:00 -0700

    Update QuestData.lua

--- GPO/Data/QuestData.lua
+++ GPO/Data/QuestData.lua
@@ -1,8 +1,92 @@
-local Data = {
-          Daph = {
-            Pos = "", -- Optional But Should Deafult to this if cannt find npcc
-            MobName = "Bandit",
-            level = 1 - 5, -- do this quest only for level 1 through 5
-            QuestName  = "Help Daph"
-  }
+local QuestData = {
+    {
+        NPC = "Daph",
+        QuestName = "Help Daph",
+        MobName = "Bandit",
+        LevelReq = 1,
+        Pos = Vector3.new(0, 0, 0), -- Default position, you'll need to fill these in
+        Enabled = false
+    },
+    {
+        NPC = "Ronny",
+        QuestName = "Help Ronny",
+        MobName = "Bandit Boss",
+        LevelReq = 5,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false
+    },
+    {
+        NPC = "Noah",
+        QuestName = "Help Noah",
+        MobName = "Besert Bandit",
+        LevelReq = 10,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false
+    },
+    {
+        NPC = "Tyrone",
+        QuestName = "Take down Lucid",
+        MobName = "Lucid",
+        LevelReq = 15,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false
+    },
+    {
+        NPC = "Robert",
+        QuestName = "Help Robert",
+        MobName = "Corrupt Marine",
+        LevelReq = 20,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false
+    },
+    {
+        NPC = "Kevin",
+        QuestName = "Help Kevin", 
+        MobName = "Shell's Bandit",
+        LevelReq = 25,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false
+    },
+    {
+        NPC = "Gozen",
+        QuestName = "Serve Justice",
+        MobName = "Axe Hand Logan",
+        LevelReq = 29,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Zen",
+        QuestName = "Help Zen",
+        MobName = "Zou Inhabitant",
+        LevelReq = 30,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Chef Rice",
+        QuestName = "Help Rice",
+        MobName = "Krieg Pirate",
+        LevelReq = 40,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Gonny",
+        QuestName = "Help gonny",
+        MobName = "Monkey",
+        LevelReq = 55,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Max",
+        QuestName = "Help max",
+        MobName = "Gorilla King",
+        LevelReq = 65,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    }
 }
+
+return QuestData
