dbd7ee12d858992137131258f3373cf5da69e6b2
7PXS <godx00@outlook.com>
2025-05-21 11:55:24 -0700

    Update QuestData.lua

--- GPO/Data/QuestData.lua
+++ GPO/Data/QuestData.lua
@@ -1,10 +1,10 @@
-local QuestData = {
+return {
     {
         NPC = "Daph",
         QuestName = "Help Daph",
         MobName = "Bandit",
         LevelReq = 1,
-        Pos = Vector3.new(0, 0, 0), -- Default position, you'll need to fill these in
+        Pos = Vector3.new(0, 0, 0), 
         Enabled = false
     },
     {
@@ -20,7 +20,7 @@ local QuestData = {
         QuestName = "Help Noah",
         MobName = "Besert Bandit",
         LevelReq = 10,
-        Pos = Vector3.new(0, 0, 0),
+        Pos = Vector3.new(-1710.1951904296875, 4.0695037841796875, -3377.113037109375),
         Enabled = false
     },
     {
@@ -28,7 +28,7 @@ local QuestData = {
         QuestName = "Take down Lucid",
         MobName = "Lucid",
         LevelReq = 15,
-        Pos = Vector3.new(0, 0, 0),
+        Pos = Vector3.new(-1842.3890380859375, 10.64361572265625, -3420.771484375),
         Enabled = false
     },
     {
@@ -36,7 +36,7 @@ local QuestData = {
         QuestName = "Help Robert",
         MobName = "Corrupt Marine",
         LevelReq = 20,
-        Pos = Vector3.new(0, 0, 0),
+        Pos = Vector3.new(-1445.1373291015625, 10.153305053710938, -5102.67041015625),
         Enabled = false
     },
     {
@@ -44,7 +44,7 @@ local QuestData = {
         QuestName = "Help Kevin", 
         MobName = "Shell's Bandit",
         LevelReq = 25,
-        Pos = Vector3.new(0, 0, 0),
+        Pos = Vector3.new(-1219.3885498046875, 63.72068786621094, -5194.44482421875),
         Enabled = false
     },
     {
@@ -52,7 +52,7 @@ local QuestData = {
         QuestName = "Serve Justice",
         MobName = "Axe Hand Logan",
         LevelReq = 29,
-        Pos = Vector3.new(0, 0, 0),
+        Pos = Vector3.new(-1364.70654296875, 84.98177337646484, -5383.00439453125),
         Enabled = false 
     },
     {
@@ -60,7 +60,7 @@ local QuestData = {
         QuestName = "Help Zen",
         MobName = "Zou Inhabitant",
         LevelReq = 30,
-        Pos = Vector3.new(0, 0, 0),
+        Pos = Vector3.new(-3172.533203125, 12.457962036132812, -5226.85791015625),
         Enabled = false 
     },
     {
@@ -68,7 +68,7 @@ local QuestData = {
         QuestName = "Help Rice",
         MobName = "Krieg Pirate",
         LevelReq = 40,
-        Pos = Vector3.new(0, 0, 0),
+        Pos = Vector3.new(-2968.32763671875, 6.600393295288086, -6697.6328125),
         Enabled = false 
     },
     {
@@ -76,7 +76,7 @@ local QuestData = {
         QuestName = "Help gonny",
         MobName = "Monkey",
         LevelReq = 55,
-        Pos = Vector3.new(0, 0, 0),
+        Pos = Vector3.new(-4246.00537109375, 42.44387435913086, -8928.9765625),
         Enabled = false 
     },
     {
@@ -84,9 +84,111 @@ local QuestData = {
         QuestName = "Help max",
         MobName = "Gorilla King",
         LevelReq = 65,
+        Pos = Vector3.new(-4268.3740234375, 42.19907760620117, -8942.791015625),
+        Enabled = false 
+    },
+    {
+        NPC = "Zhen",
+        QuestName = "Help zhen",
+        MobName = "Castle Guard",
+        LevelReq = 110,
+        Pos = Vector3.new(4095.6953125, 1818.945068359375, -9833.3017578125),
+        Enabled = false 
+    },
+    {
+        NPC = "Raze",
+        QuestName = "Help raze",
+        MobName = "Sky Guardian",
+        LevelReq = 125,
+        Pos = Vector3.new(2272.48681640625, 2081.9443359375, -10038.734375),
+        Enabled = false 
+    },
+    {
+        NPC = "Bibby",
+        QuestName = "Help bibby",
+        MobName = "Malcom's Undermen",
+        LevelReq = 135,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Viva",
+        QuestName = "Help viva",
+        MobName = "Malcom",
+        LevelReq = 140,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "jeff",
+        QuestName = "Help jeff",
+        MobName = "Thunder God",
+        LevelReq = 155,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Becky",
+        QuestName = "Help becky",
+        MobName = "Fishman Karate User",
+        LevelReq = 190,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Jenny",
+        QuestName = "Help jenny",
+        MobName = "Ryu",
+        LevelReq = 210,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Janny",
+        QuestName = "Help janny",
+        MobName = "Neptune",
+        LevelReq = 230,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Poro",
+        QuestName = "Help Poro",
+        MobName = "Zombie",
+        LevelReq = 300,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Eli",
+        QuestName = "Help Eli",
+        MobName = "Borj",
+        LevelReq = 325,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Mur",
+        QuestName = "Help Mur",
+        MobName = "Moria",
+        LevelReq = 325,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Jag",
+        QuestName = "Help Jag",
+        MobName = "Ryuma",
+        LevelReq = 350,
+        Pos = Vector3.new(0, 0, 0),
+        Enabled = false 
+    },
+    {
+        NPC = "Marine Scout Rae",
+        QuestName = "Help Marine Scout Rae",
+        MobName = "Zombie Knight",
+        LevelReq = 350,
         Pos = Vector3.new(0, 0, 0),
         Enabled = false 
     }
 }
-
-return QuestData
