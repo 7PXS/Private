8e3f8b8feeb23fed00cf5a7eb3ed8ba0e7679233
7PXS <godx00@outlook.com>
2025-05-21 12:27:36 -0700

    Create PlayerSettings.lua

---
+++ GPO/Data/MainSettings/PlayerSettings.lua
@@ -0,0 +1,24 @@
+return {
+    CFrameWalkSpeed = {
+        Enabled = false,
+        Speed = 2
+    },
+    Fly = {
+        Enabled = false,
+        Speed = 50,
+        BodyVelocity = nil
+    },
+    NoClip = {
+        Enabled = false
+    },
+    NoFall = {
+        Enabled = false
+    },
+    InfiniteJump = {
+        Enabled = false
+    },
+    Teleport = {
+        Locations = {},
+        Selected = nil
+    }
+}
