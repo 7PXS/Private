d831f1e1010aefd47f13eccbbde8735c0b32add3
7PXS <godx00@outlook.com>
2026-01-17 22:03:24 -0800

    Delete Loader directory

--- Loader/Avoura/src/loader.lua
+++
@@ -1,2 +0,0 @@
-getgenv().AVOURA_KEY = "KeyHere"
-loadstring(game:HttpGet("https://production--skider.netlify.app/Loader/Avoura/src/bknd.lua"))()
--- Loader/example.lua
+++
@@ -1,2 +0,0 @@
-getgenv().scriptkey = "KeyHere"
-loadstring(game:HttpGet("https://production--skider.netlify.app/Loader/loader.lua"))()
